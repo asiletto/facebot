c6018bf42f6ad8cc61114d7d3b93dac609f882c3
Alessandro Siletto <alessandro.siletto@unipolsai.it>
2019-01-04 12:48:05 +0100

    fix

--- src/main/java/facebot/WebDriverUtils.java
+++ src/main/java/facebot/WebDriverUtils.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class WebDriverUtils {
 
 	public static WebDriver getWebDriver(String webdriver, String profileDir){
-        System.getProperties().setProperty("webdriver.chrome.driver", "D:\\Desktop 5\\hacks\\facebot\\chromedriver.exe");
+        System.getProperties().setProperty("webdriver.chrome.driver", webdriver);
         
         DesiredCapabilities dc = DesiredCapabilities.chrome();              
 //        System.setProperty("http.proxyHost", "127.0.0.1");
